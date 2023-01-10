afeae08a499d39af437b61846923959f1306a781
SiddhantRewale <122270678+SiddhantRewale@users.noreply.github.com>
2023-01-10 09:57:26 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+for i in range (1,5):
+    if(i==1):
+        s=str(i)
+        print(s)
+    elif(i==2):
+        s=str(len(s))+str(1)
+        print(s)
+    elif(i==3):
+        s=str(len(s))+str(1)
+        print(s)
+    elif(i==4):
+        str(s)
+        s=str((s.count(2))+str(1))    
+    
